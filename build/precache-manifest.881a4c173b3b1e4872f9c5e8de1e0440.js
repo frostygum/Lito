b9bffff55bf0b445e08aa054baf60ab37b473f0d
frostygum <4nthonius.juan5@gmail.com>
2020-08-24 18:47:17 +0700

    add build files

---
+++ build/precache-manifest.881a4c173b3b1e4872f9c5e8de1e0440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9c8084ce93446c36b8215680bdaf64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18be9ef86f59da43f6f3",
+    "url": "/static/css/main.fca33bc5.chunk.css"
+  },
+  {
+    "revision": "4fd55db0efe165f4a498",
+    "url": "/static/js/2.be983ef1.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.be983ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18be9ef86f59da43f6f3",
+    "url": "/static/js/main.c0eeba4c.chunk.js"
+  },
+  {
+    "revision": "e517c5ec56a216318bd1",
+    "url": "/static/js/runtime-main.e67a83ad.js"
+  }
+]);
